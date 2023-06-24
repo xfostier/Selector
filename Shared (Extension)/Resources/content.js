4f9ce7293421c4ff34b72ccc80bf9921299635fb
Xavier Fostier <xavier.fostier@mailfence.com>
2023-06-24 22:04:26 +0200

    Able to change selection background (no persistence)

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,36 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+// Ressources : https://www.vshsolutions.com/blogs/how-to-write-your-first-ios-safari-extension/
+// https://developer.mozilla.org/fr/
+
+let color = '#FA96A7'; // Keeping pink as default on first page load
+
+const changeColor = () => {
+    var selection
+    if (window.getSelection) {
+        selection = window.getSelection()
+        if (selection.collapsed) {
+            console.log("Selection is empty")
+            return
+        }
+        if (selection.rangeCount <= 0) {
+            console.log("No range in selection")
+            return
+        }
+        var range = selection.getRangeAt(0);
+        let newNode = document.createElement("selector");
+        range.surroundContents(newNode);
+        newNode.style.backgroundColor = color;
+        console.log(offset);
+        console.log(range);
+        console.log(selection);
+        console.log(selection.toString());
+        return selection
+    }
+    console.log("Failed to get selection")
+}
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+    if(request.color) {
+        color = request.color
+        changeColor()
+    }
 });
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,28 @@
-console.log("Hello World!", browser);
+const colors = [
+{
+name: "Pink",
+code: '#FA96A7'
+},{
+name: "Orange",
+code: '#F28500'
+},{
+name: "Yellow",
+code: '#fc3'
+},{
+name: "Grey",
+code: '#A2A9B1'
+}]
+
+const sendColor = async (color) => {
+const [tab] = await browser.tabs.query({currentWindow: true, active: true})
+chrome.tabs.sendMessage(tab.id, { color })
+}
+colors.forEach(color => {
+const button = document.createElement('button')
+button.style.background = color.code
+button.innerText = color.name
+document.querySelector('#color-container').appendChild(button)
+button.addEventListener('click', e => {
+sendColor(color.code)
+})
+})
